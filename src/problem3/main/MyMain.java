c8cf6096e99351c33a9fc8df44bc8586dbea4a0e
tushar-manish <tushargupta9412@gmail.com>
2020-03-24 23:30:23 -1000

    Execution Class

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,19 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("tushar",1);
+        obj.insert("gupta",4);
+        obj.insert("gupta",3);
+        obj.insert("gupta",2);
+        obj.insert("tushar",5);
+        obj.display();
+    }
 }
