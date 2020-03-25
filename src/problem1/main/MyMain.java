c742fd3394b5ea989fc635d9b3f94ab999644798
tushar-manish <tushargupta9412@gmail.com>
2020-03-24 18:11:39 -1000

    Execution Class

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,17 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(5);
+        obj.insert(1);
+        obj.insert(10);
+        obj.insert(4);
+        obj.insert(6);
+        obj.display();
+
+    }
 }
