b6cafb4b070268e29623191dd48132c1eced5f08
tushar-manish <tushargupta9412@gmail.com>
2020-03-24 16:43:32 -1000

    Tree Node class with data,left,right

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,16 @@
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
+    public void setdata(int data){
+        this.data = data;
+    }
+    public int getdata(){
+        return data;
+    }
+    public void setRight(){
+        left =
+    }
 }
