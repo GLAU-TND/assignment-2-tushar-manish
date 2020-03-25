e12a4e0723cc9980afe47f874736fa5ab7511850
tushar-manish <tushargupta9412@gmail.com>
2020-03-24 18:47:46 -1000

    Node class with Student object

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,52 @@
  */
 package problem3.node;
 
+class Student{
+    private String name;
+    private int roll;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", roll=" + roll +
+                '}';
+    }
+}
+
 public class Node {
+    Student stu;
+    Node next;
+
+    public void setStu(Student stu) {
+        this.stu = stu;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Student getStu() {
+        return stu;
+    }
+
+    public Node getNext() {
+        return next;
+    }
 }
