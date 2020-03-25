5d12db1a2b75c8d98ee4e403939fb2a2833d67f4
tushar-manish <tushargupta9412@gmail.com>
2020-03-24 22:06:26 -1000

    Implementation class

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,28 @@
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+import problem5.student.Student;
+
 public class MyPriorityQueue {
+    Node front;
+    Node rear;
+    MyPriorityQueue(){
+        front = null;
+        rear = null;
+    }
+
+    public void insert(Student st){
+        Node ns = new Node();
+        ns.setStu(st);
+        if(rear==null && front==null){
+            front = ns;
+            rear = ns;
+        }
+        else{
+            rear.setNext(ns);
+            rear = ns;
+        }
+    }
+
 }
