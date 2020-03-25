a595e2136792646216c1c24395c0073cbc02e826
tushar-manish <tushargupta9412@gmail.com>
2020-03-24 17:45:03 -1000

    Implementation class (insert,printleft,display)

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,47 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    TreeNode root;
+
+    MyBinarySearchTree(){
+        root = null;
+    }
+
+    public void insert(int data){
+        TreeNode ns = new TreeNode();
+        ns.setdata(data);
+        if(root == null){
+            root = ns;
+        }
+        else{
+            TreeNode current = root;
+            TreeNode previous = null;
+            while(true){
+                previous = current;
+                if(data > current.getdata()){
+                    if(current.getRight()==null){
+                        current.setRight(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getRight();
+                    }
+                }
+                else{
+                    if(current.getLeft()==null){
+                        current.setLeft(ns);
+                        break;
+                    }
+                    else{
+                        current = current.getLeft();
+                    }
+                }
+            }
+        }
+    }
 }
