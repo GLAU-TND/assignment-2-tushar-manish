cd82148272970acba0c838f1d14a28a3bfcfb975
tushar-manish <tushargupta9412@gmail.com>
2020-03-26 09:09:45 -1000

    Student class with 4 attribute

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,15 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    int roll;
+    String name;
+    int marks;
+    int backlog;
+
+    public Student(int roll, String name, int marks, int backlog) {
+        this.roll = roll;
+        this.name = name;
+        this.marks = marks;
+        this.backlog = backlog;
+    }
 }
